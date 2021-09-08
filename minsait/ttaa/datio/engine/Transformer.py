e8b6bf977fa0cb033aedb122c350bf7ba5fc3f81
Devis Bernal <devis.bernal.contractor@bbva.com>
2021-09-08 13:13:37 -0500

    Entrega de diagnostico

--- minsait/ttaa/datio/engine/Transformer.py
+++ minsait/ttaa/datio/engine/Transformer.py
@@ -12,9 +12,11 @@ class Transformer(Writer):
         self.spark: SparkSession = spark
         df: DataFrame = self.read_input()
         df.printSchema()
-        df = self.clean_data(df)
-        df = self.example_window_function(df)
         df = self.column_selection(df)
+        df = self.example_window_function(df)
+        df = self.get_potential_vs_overall(df)
+        df = self.evaluate_players(df)
+        self.test_evaluate_players_player_cat(df)
 
         # for show 100 records after your transformations and show the DataFrame schema
         df.show(n=100, truncate=False)
@@ -32,19 +34,45 @@ class Transformer(Writer):
             .option(HEADER, True) \
             .csv(INPUT_PATH)
 
-    def clean_data(self, df: DataFrame) -> DataFrame:
+    def evaluate_players(self, df: DataFrame) -> DataFrame:
         """
         :param df: is a DataFrame with players information
         :return: a DataFrame with filter transformation applied
         column team_position != null && column short_name != null && column overall != null
         """
         df = df.filter(
-            (short_name.column().isNotNull()) &
-            (short_name.column().isNotNull()) &
-            (overall.column().isNotNull()) &
-            (team_position.column().isNotNull())
+            (player_cat.column().isin("A","B")) |
+            ((player_cat.column() == "C") & (potential_vs_overall.column() > 1.15)) |
+            ((player_cat.column() == "D") & (potential_vs_overall.column() > 1.25))
         )
         return df
+
+
+    def test_data(df1: DataFrame, df2: DataFrame):
+            """
+            Function for comparing two DataFrame data. If data is equal returns True.
+            :param df1: test DataFrame :param df2: test DataFrame
+            :return: Boolean
+            """
+
+            data1 = df1.collect()
+            data2 = df2.collect()
+            return set(data1) == set(data2)
+
+    def test_evaluate_players_player_cat(self, df: DataFrame):
+        evaluated_df=df.select(player_cat.column()).dropDuplicates()
+        expected_Df=self.spark.createDataFrame(
+            data=[['A'],['B'],
+                  ['C'],['D']],
+            schema=['player_cat'])
+        self.asserTrue(self.test_data(evaluated_df, expected_Df))
+
+
+
+    def test_evaluate_players_player_cat_C(self, df: DataFrame):
+        test_df= df.filter(player_cat.column()=="C").select(potential_vs_overall.column())
+        wrong_df_filter=test_df.filter(potential_vs_overall.column()<=1.15)
+        self.assert wrong_df_filter is None
 
     def column_selection(self, df: DataFrame) -> DataFrame:
         """
@@ -53,10 +81,15 @@ class Transformer(Writer):
         """
         df = df.select(
             short_name.column(),
-            overall.column(),
+            long_name.column(),
+            age.column(),
             height_cm.column(),
+            weight_kg.column(),
+            nationality.column(),
+            club_name.column(),
+            overall.column(),
+            potential.column(),
             team_position.column(),
-            catHeightByPosition.column()
         )
         return df
 
@@ -70,13 +103,26 @@ class Transformer(Writer):
              cat C for the rest
         """
         w: WindowSpec = Window \
-            .partitionBy(team_position.column()) \
-            .orderBy(height_cm.column().desc())
+            .partitionBy(nationality.column(),\
+                        team_position.column()) \
+            .orderBy(overall.column().asc())
         rank: Column = f.rank().over(w)
 
-        rule: Column = f.when(rank < 10, "A") \
-            .when(rank < 50, "B") \
-            .otherwise("C")
+        rule: Column = f.when(rank <= 3, "A") \
+            .when(rank <= 5, "B") \
+            .when(rank <= 10, "C") \
+            .otherwise("D")
 
-        df = df.withColumn(catHeightByPosition.name, rule)
+        df = df.withColumn(player_cat.name, rule)
+        return df
+
+    def get_potential_vs_overall(self, df: DataFrame) -> DataFrame:
+        """
+        :param df: is a DataFrame with players information
+        :return: a DataFrame add potential_vs_overall column
+        """
+        df = df.withColumn(
+            potential_vs_overall.name,
+            potential.column()/overall.column()
+        )
         return df
